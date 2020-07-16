284d550574f96faa24ae1a1d2c8fe10ade0d1b91
dololgun <dololgun@gmail.com>
2020-07-16 23:19:56 +0900

    create builder and visitor

--- com.sam.e4.minimark.ui/src/com/sam/e4/minimark/ui/MinimarkBuilder.java
+++ com.sam.e4.minimark.ui/src/com/sam/e4/minimark/ui/MinimarkBuilder.java
@@ -3,42 +3,68 @@ package com.sam.e4.minimark.ui;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 /**
+ * <p>
+ * 소스가 변경되면 이클립스는 프레임워크는 빌드 이벤트를 발생한다. 이 빌드 이벤트를 받기 위해
+ * IncrementalProjectBuilder를 상속받은 builder를 만든다. IncrementalProjectBuilder를 추가하기
+ * 위해서는 org.eclipse.core.resources를 디펜던시에 추가해야 한다. builder를 hook up하기 위해서
+ * org.eclipse.core.resources.builders를 extension point로 추가해야 한다.
+ * 
+ * <P>
+ * incrementalBuilder는 file delete에 대해서 동작하지 않는다. file delete를 감지하기 위해 delta의
+ * 체크하여 파일 변경의 종류를 찾아야 한다. 삭제애 대한 처리를 하지 않으면 파일 삭제시 오류가 발생한다.
+ * 
  * @author leegeonho
- * 소스가 변경되면 이클립스는 프레임워크는 빌드 이벤트를 발생한다.
- * 이 빌드 이벤트를 받기 위해  IncrementalProjectBuilder를 상속받은 builder를 만든다. 
- * IncrementalProjectBuilder를 추가하기 위해서는 org.eclipse.core.resources를 디펜던시에 추가해야 한다.
- * builder를 hook up하기 위해서 org.eclipse.core.resources.builders를 extension point로 추가해야 한다.
- *
  */
 public class MinimarkBuilder extends IncrementalProjectBuilder {
 
-	@Override 
+	@Override
 	protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException {
-		// 
+		//
 		if (kind == FULL_BUILD) {
+			// 전체파일을 빌드한다.
 			fullBuild(getProject(), monitor);
 		} else {
+			// 변경된 파일만 빌드한다.
 			incrementalBuild(getProject(), monitor, getDelta(getProject()));
 		}
 		return null;
 	}
-	
-	private void incrementalBuild(IProject project, IProgressMonitor monitor, IResourceDelta delta) throws CoreException {
+
+	private void incrementalBuild(IProject project, IProgressMonitor monitor, IResourceDelta delta)
+			throws CoreException {
 		if (delta == null) {
-			
+			fullBuild(project, monitor);
 		} else {
-			System.out.println("Doing an incremental build");
+//			System.out.println("Doing an incremental build");
+			delta.accept(new MinimarkVisitor());
 		}
 	}
-	
+
 	private void fullBuild(IProject project, IProgressMonitor monitor) throws CoreException {
-		System.out.println("Doing a full build");
+//		System.out.println("Doing a full build");
+		project.accept(new MinimarkVisitor(), IResource.NONE);
+	}
+
+	/**
+	 * 프로젝트를 clean할때 원본 소스와 연관된 파일은 삭제되어야 한다.
+	 */
+	@Override
+	protected void clean(IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+		super.clean(monitor);
+		deleteAll(getProject(), monitor);
+	}
+	
+	private void deleteAll(IProject project, IProgressMonitor monitor) throws CoreException {
+//		TODO ???
+//		project.accept(new MinimarkVisitor(), IResource.NONE);
 	}
 
 }
---
+++ com.sam.e4.minimark.ui/src/com/sam/e4/minimark/ui/MinimarkVisitor.java
@@ -0,0 +1,119 @@
+package com.sam.e4.minimark.ui;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IResourceProxy;
+import org.eclipse.core.resources.IResourceProxyVisitor;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+
+public class MinimarkVisitor implements IResourceProxyVisitor, IResourceDeltaVisitor {
+
+	/**
+	 * 프레임웍에서 파일들을 방문할 때 발생하는 이벤트
+	 */
+	@Override
+	public boolean visit(IResourceProxy proxy) throws CoreException {
+		//
+		String name = proxy.getName();
+		if (name != null && name.endsWith(".minimark")) {
+//			System.out.println("Processing " + name);
+			processResource(proxy.requestResource());
+		}
+
+		// 자식 리소스를 계속 방문하기 위해 true를 리턴한다.
+		return true;
+	}
+
+	/**
+	 * 델타를 통해 변경된 파일만 빌드하는 과정
+	 */
+	@Override
+	public boolean visit(IResourceDelta delta) throws CoreException {
+		
+		// 삭제 이벤트인 경우
+		boolean deleted = (IResourceDelta.REMOVED & delta.getKind()) != 0;
+		
+		// 델타로부터 리소를 얻는다. 
+		IResource resource = delta.getResource();
+		String name = resource.getName(); 
+		if (name.endsWith(".minimark")) {
+			System.out.println("incremental build");
+			if (deleted) {
+				String htmlName = name.replace(".minimark", ".html");
+				IFile htmlFile = resource.getParent().getFile(new Path(htmlName));
+				if (htmlFile.exists()) {
+					htmlFile.delete(true, null);
+				}
+			} else {
+				processResource(resource);
+			}
+		}
+		
+		// html 파일을 삭제한 경우 이에 대응하는 .minimark 파일이 존재한다면 html이 다시 생성되어야 한다.
+		// html 을 사용자가 강제로 수정하는 경우도 마찬가지이다.
+		if (name.endsWith(".html")) {
+			String minimarkName = name.replace(".html", ".minimark");
+			IFile minimarkFile = resource.getParent().getFile(new Path(minimarkName));
+			if (minimarkFile.exists()) {
+				processResource(minimarkFile);
+			}
+		}
+		return true;
+	}
+
+	private void processResource(IResource resource) throws CoreException {
+
+		/*
+		 *  파일 삭제 이벤트가 발생할 경우에 대비하여 리소스의 존재여부를 체크하여 nullpointerException을 방지한다
+		 *  이렇게 하여도 이미 생성된 html파일은 자동으로 삭제 되지 않는다. 
+		 *  
+		 */
+		if (resource instanceof IFile && resource.exists()) {
+			IFile file = (IFile) resource;
+
+			// 생성할 파일의 이름을 정하자
+			System.out.println("file.getName() = " + file.getName());
+			String htmlName = file.getName().replace(".minimark", ".html");
+
+			// 원본파일로 부터 컨테이너를 얻는다.
+			IContainer container = file.getParent();
+
+			// 컨테이너로 부터 새로 생성할 파일을 얻는다.
+			IFile htmlFile = container.getFile(new Path(htmlName));
+			InputStream in = file.getContents();
+			try {
+//				MinimarkTranslator.convert(new InputStreamReader(in), new OutputStreamWriter(System.out));
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				MinimarkTranslator.convert(new InputStreamReader(in), new OutputStreamWriter(baos));
+
+				ByteArrayInputStream contents = new ByteArrayInputStream(baos.toByteArray());
+
+				if (htmlFile.exists()) {
+					htmlFile.setContents(contents, true, false, null);
+				} else {
+					htmlFile.create(contents, true, null);
+				}
+				
+				// 이클립스에게 htmlFile이 도출됨을 알린다.
+				// 사용자가 편집한 것이 아니고 자동으로 생성된 것이라는 의미 
+				htmlFile.setDerived(true, null);
+				
+			} catch (IOException e) {
+				throw new CoreException(new Status(Status.ERROR, Activator.PLUGIN_ID, "Fail to generate resource", e));
+			}
+		}
+	}
+
+}
